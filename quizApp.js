727619ad79c46bc8ea31f23692e7dd175383d2af
Stephen Done <srdone@gmail.com>
2015-01-21 19:32:48 -0700

    Finished quizApp.js

--- quizApp.js
+++ quizApp.js
@@ -6,35 +6,54 @@
 //Create a User constructor that accepts name, email, password, and totalScore parameters and set them appropriatly
 
   //code here
+  var User = function(name, email, password, totalScore) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.totalScore = totalScore;
+  };
 
 
 //Create a Question constructor that accepts title, answersArray, rightAnswer, and difficulty parameters
 
   //code here
+  var Question = function(title, answersArray, rightAnswer, difficulty) {
+    this.title = title;
+    this.answersArray = answersArray;
+    this.rightAnswer = rightAnswer;
+    this.difficulty = difficulty;
+  };
 
 
 //Create a users Array which is going to hold all of our users.
 
   //code here
-
+  var users = [];
 
 //Let's say three people signed up for our service, create 3 instances of User and add each to the users Array
 
   //code here
-
+  users.push(new User("Stephen", "srdone@gmail.com", "asdf", 0));
+  users.push(new User("Fred", "fred@gmail.com", "asdflkje", 0));
+  users.push(new User("Fanny", "fanny@gmail.com", "asde", 0));
 
 //Create a questions Array which is going to hold all of our questions
 
   //code here
+  var questions = [];
 
 
 //Now, let's say we wanted to create a quiz about JavaScript. Create three instances of Question which contain the following data
-//title: 'T/F: Inheritance is achieved in JavaScript through Prototypes?'
-//title: 'T/F: JavaScript is just a scripting version of Java'
-//title: "T/F: In Javascript, == doesn't check 'type' but just the value - where === checks type and value"
+var q1title = 'T/F: Inheritance is achieved in JavaScript through Prototypes?';
+var q2title = 'T/F: JavaScript is just a scripting version of Java';
+var q3title = "T/F: In Javascript, == doesn't check 'type' but just the value - where === checks type and value";
+var tfarr = ["true", "false"];
 //Fill in the rest of the required data as you see appropriate.
 
   //code here
+  questions.push(new Question(q1title, tfarr, "true", "medium"));
+  questions.push(new Question(q2title, tfarr, "false", "easy"));
+  questions.push(new Question(q3title, tfarr, "true", "medium"));
 
 
 //Now push all of your instances of Question into the questions Array
@@ -45,4 +64,10 @@ console.log('My users Array and my questions arrray are ...');
 //Now loop console.log your users array and your questions array and verify that they're both holding the right data.
 
   //code here
+  users.forEach(function(current) {
+    console.log(current);
+  });
+  questions.forEach(function(current) {
+    console.log(current);
+  });
 
